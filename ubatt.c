5097b4a10751fceb123055adf993981613cdf8f2
Stephen Cavilia <sac@atomicradi.us>
2020-09-07 12:10:46 -0400

    show laptop battery charging status, -V flag for daemon version

--- ubatt.c
+++ ubatt.c
@@ -2,6 +2,14 @@
 #include <glib.h>
 #include <upower.h>
 
+static gboolean opt_ver;
+
+static GOptionEntry entries[] =
+{
+    {"version", 'V', 0, G_OPTION_ARG_NONE, &opt_ver, "Show UPower daemon version", NULL},
+    NULL
+};
+
 void ubatt_show_on_battery(UpClient *upower)
 {
     if (up_client_get_on_battery(upower))
@@ -21,10 +29,8 @@ UpDevice *ubatt_find_laptop_battery(UpClient *upower)
 
     for (int i=0; i<devices->len; i++)
     {
-        g_print("Device %d\n", i);
         UpDevice *this_dev = g_ptr_array_index(devices, i);
         gchar *text = up_device_to_text (this_dev);
-        //g_print ("%s\n", text);
         gboolean power_supply;
         UpDeviceKind kind;
         g_object_get(this_dev,
@@ -41,9 +47,68 @@ UpDevice *ubatt_find_laptop_battery(UpClient *upower)
     return device;
 }
 
+void ubatt_show_battery(UpDevice *battery)
+{
+    UpDeviceKind kind;
+    gdouble percent;
+    UpDeviceState state;
+    const gchar *label, *statelabel;
+
+    g_object_get(battery,
+                 "kind", &kind,
+                 "percentage", &percent,
+                 "state", &state,
+                 NULL);
+
+    switch (kind)
+    {
+    case UP_DEVICE_KIND_BATTERY:
+        label = "Battery";
+        break;
+    case UP_DEVICE_KIND_UPS:
+        label = "UPS";
+        break;
+    case UP_DEVICE_KIND_MOUSE:
+        label = "Mouse";
+        break;
+    default:
+        label = up_device_kind_to_string(kind);
+    }
+
+    switch (state)
+    {
+    case UP_DEVICE_STATE_CHARGING:
+        statelabel = "Charging";
+        break;
+    case UP_DEVICE_STATE_DISCHARGING:
+        statelabel = "Discharging";
+        break;
+    case UP_DEVICE_STATE_EMPTY:
+        statelabel = "Empty";
+        break;
+    case UP_DEVICE_STATE_FULLY_CHARGED:
+        statelabel = "Fully charged";
+        break;
+    default:
+        statelabel = up_device_state_to_string(state);
+    }
+
+    g_print("%s: %d%%, %s\n", label, (int)percent, statelabel);
+}
+
 int main(int argc, char **argv)
 {
     UpClient *upower;
+    GError *error = NULL;
+    GOptionContext *opts;
+
+    opts = g_option_context_new("- show upower battery status");
+    g_option_context_add_main_entries(opts, entries, NULL);
+    if (!g_option_context_parse(opts, &argc, &argv, &error))
+    {
+        g_print("command line error: %s\n", error->message);
+        return 1;
+    }
 
     upower = up_client_new();
 
@@ -53,9 +118,14 @@ int main(int argc, char **argv)
         return 2;
     }
 
-    const gchar *up_ver = up_client_get_daemon_version(upower);
+    if (opt_ver)
+    {
+        const gchar *up_ver = up_client_get_daemon_version(upower);
 
-    g_print("Upower version: %s\n", up_ver);
+        g_print("UPower version: %s\n", up_ver);
+
+        return 0;
+    }
 
     ubatt_show_on_battery(upower);
 
@@ -63,8 +133,14 @@ int main(int argc, char **argv)
     if (battery)
     {
         gdouble percent;
-        g_object_get(battery, "percentage", &percent, NULL);
-        g_print("Found battery: %.1f%%\n", percent);
+        UpDeviceState state;
+        g_object_get(battery,
+                     "percentage", &percent,
+                     "state", &state,
+                     NULL);
+        //g_print("Found battery: %.1f%% %s\n", percent, up_device_state_to_string(state));
+
+        ubatt_show_battery(battery);
 
         g_object_unref(battery);
     }
